9916f211361e5ade95a6e9dfa9fa749550907b3a
Paweł <61346706+pawelgajger@users.noreply.github.com>
2024-11-01 14:28:44 +0100

    fix all

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Dodaj klasę 'active' do elementów '.scroll-animate' po przewinięciu do widoku
 window.addEventListener('scroll', function () {
 	const elements = document.querySelectorAll('.scroll-animate')
 	elements.forEach(element => {
@@ -10,18 +11,10 @@ window.addEventListener('scroll', function () {
 	})
 })
 
-// Po załadowaniu strony dodajemy klasę 'appear' do elementów z klasą 'fade-in-element'
+// Po załadowaniu strony dodaj klasę 'appear' do elementów z klasą 'fade-in-element'
 document.addEventListener('DOMContentLoaded', () => {
 	const fadeElements = document.querySelectorAll('.fade-in-element')
 	fadeElements.forEach(element => {
 		element.classList.add('appear')
-	})
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-	const menuItems = document.querySelectorAll('.navbar ul li')
-	menuItems.forEach((item, index) => {
-		item.style.animationDelay = `${index * 0.2}s` // Automatyczne opóźnienie dla każdego elementu
-		item.classList.add('fade-in-element') // Dodaje animację fade-in
 	})
 })
